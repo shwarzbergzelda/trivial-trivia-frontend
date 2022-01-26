492e01346e5de65f0be4f4a528392d64ef384ca9
zelda <zeldashw@gmail.com>
2022-01-26 06:30:49 -0500

    added more basic text to quiz results page

--- src/Components/QuizResults.js
+++ src/Components/QuizResults.js
@@ -1,11 +1,16 @@
 import React, { useContext } from 'react'
 import { Context } from './Context'
-
+import { Link } from 'react-router-dom'
 export default function QuizResults(props) {
-    const { correctAnswersCount } = useContext(Context)
+    const { correctAnswersCount, numberOfQuizQuestions } = useContext(Context);
+
     return (
         <div>
-            {correctAnswersCount}
+            <h1>Quiz Results</h1>
+            <h3>Total Points</h3>
+            <h5>{correctAnswersCount}/{numberOfQuizQuestions}</h5>
+            <button><Link to="/Category">Take Another Quiz</Link></button>
+            <button><Link to="/Leaderboard">Leaderboard</Link></button>
         </div>
     )
 }
