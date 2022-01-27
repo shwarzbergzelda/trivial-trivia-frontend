12e984ca958b3464a4de1f0b8b761f7dc7e6b7c6
zelda <zeldashw@gmail.com>
2022-01-27 17:12:43 -0500

    edited navbar styling, changed page title, and favicon

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,19 +2,17 @@ import React from 'react';
 import logo from '../images/logo.png'
 import profile from '../images/Profile.png'
 import { Link } from 'react-router-dom'
+import "../styles/Navbar.css"
 
 export default function Navbar() {
     return(
         <>
             <nav className='navbar'>
-            <Link to='/'><img className='logo' src={logo} alt="Logo"></img></Link>
-                <div>
-                <Link to='/Profile'><img className='profile' src={profile} alt="Profile"></img></Link>
-                    <div className='leaderboard-button'>
-                    <Link to='/Leaderboard'>Leaderboard</Link>
-                    </div>
+                <Link to='/'><img className='logo' src={logo} alt="Logo"></img></Link>
+                <div className="leaderboard-profile">
+                    <div className="leaderboard-button"><Link to='/Leaderboard'>Leaderboard</Link></div>
+                    <Link to='/Profile'><img className='profile' src={profile} alt="Profile"></img></Link>
                 </div>
-                
             </nav>
         </>
     )
